42f70e24cac8b4cc83ddc799e7c184ff22f28d2b
Steph <S.Kuunal1@newcastle.ac.uk>
2019-03-20 11:11:00 +0000

    Added y labels to my plots and started a table of contents

--- myscript.R
+++ myscript.R
@@ -5,6 +5,7 @@ summary(iris)
 
 plot(Sepal.Length ~ Sepal.Width, data = iris)
 
+## creating plots ####
 library(ggplot2)
 ggplot(iris, aes(x= Petal.Length, y= Petal.Width)) +
   geom_point() +
@@ -12,4 +13,10 @@ ggplot(iris, aes(x= Petal.Length, y= Petal.Width)) +
 
 ggplot(iris, aes(x= Species, y= Petal.Length)) +
   geom_boxplot() +
-  theme_classic()
+  theme_classic()+
+  ylab("Petal Length")
+
+ggplot(iris, aes(x= Species, y= Petal.Width)) +
+  geom_boxplot() +
+  theme_classic() +
+  ylab("Petal Width")
