7ca24e198b031e82a060a5920ee0b18a95ac7a4a
Steph <S.Kuunal1@newcastle.ac.uk>
2019-03-20 11:06:47 +0000

    Improved boxplot of species and petal length

--- myscript.R
+++ myscript.R
@@ -4,10 +4,12 @@ data("iris")
 summary(iris)
 
 plot(Sepal.Length ~ Sepal.Width, data = iris)
-plot(iris$Species, iris$Petal.Length, data = iris)
 
 library(ggplot2)
 ggplot(iris, aes(x= Petal.Length, y= Petal.Width)) +
   geom_point() +
   theme_classic()
 
+ggplot(iris, aes(x= Species, y= Petal.Length)) +
+  geom_boxplot() +
+  theme_classic()
