75ac0ea8b4e87f110ee2bf5447f46ebf3261e75e
Steph <S.Kuunal1@newcastle.ac.uk>
2019-03-20 11:13:40 +0000

    Created new plot for sepal length vs width and deleted the old plot

--- myscript.R
+++ myscript.R
@@ -1,9 +1,7 @@
-##iris data
+##iris data read in ####
 
 data("iris")
 summary(iris)
-
-plot(Sepal.Length ~ Sepal.Width, data = iris)
 
 ## creating plots ####
 library(ggplot2)
@@ -20,3 +18,9 @@ ggplot(iris, aes(x= Species, y= Petal.Width)) +
   geom_boxplot() +
   theme_classic() +
   ylab("Petal Width")
+
+ggplot(iris, aes(x= Sepal.Length, y= Sepal.Width)) +
+  geom_point() +
+  theme_classic() +
+  ylab("Sepal Width") +
+  xlab("Sepal Length")
