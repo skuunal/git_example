a0d5046382c7ecf8db3c37f7c85e38989d3f7710
Steph <S.Kuunal1@newcastle.ac.uk>
2019-03-20 11:03:35 +0000

    GGplot with nicer output

--- myscript.R
+++ myscript.R
@@ -4,3 +4,10 @@ data("iris")
 summary(iris)
 
 plot(Sepal.Length ~ Sepal.Width, data = iris)
+plot(iris$Species, iris$Petal.Length, data = iris)
+
+library(ggplot2)
+ggplot(iris, aes(x= Petal.Length, y= Petal.Width)) +
+  geom_point() +
+  theme_classic()
+
